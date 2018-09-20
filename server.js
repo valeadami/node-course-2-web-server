6290a21bab372ce4f798333d0db66cb17144c557
Valentina Adami <valeadami@gmail.com>
2018-09-20 20:02:41 +0200

    modificato server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
-  //console.log(log);
+  console.log(log);
   fs.appendFile('server.log', log + '\n', (err) => {
     if (err) {
       console.log('Unable to append to server.log.');
